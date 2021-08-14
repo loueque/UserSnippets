6958b4b8c9da5e7971fddfe837fa07eb6cec4521
loueque <66234951+loueque@users.noreply.github.com>
2021-08-14 14:09:58 -0700

    Rewrote script

--- User_Snippets/Main.lua
+++ User_Snippets/Main.lua
@@ -1,83 +1,71 @@
+local plugin: Plugin = plugin
+
 local ChangeHistoryService = game:GetService("ChangeHistoryService")
 local SoundService = game:GetService("SoundService")
 local RunService = game:GetService("RunService")
-local user = UserSettings()
-
-local Props = require(script:WaitForChild("Properties"))
 
 local toolbar = plugin:CreateToolbar("User Snippets")
-local genScript = toolbar:CreateButton("Generate Sippet", "Type a specific command like 'class' to generate a snippet!", "rbxassetid://6827337191")
-local packScript = toolbar:CreateButton("Settings", "Set custom snippets, or configure code", "rbxassetid://6827337139")
+local activation = toolbar:CreateButton("Activate", "Activates protocall of user snippets", "rbxassetid://6841215820")
+activation.ClickableWhenViewportHidden = true
 
 function main()
 	local selection = game:GetService("Selection")
 	local current = selection:Get()
-
-	local function f(int: any)
-		return Props:GetProperties(int)
-	end
-
 	if current[1] then
-		local s = Instance.new("ModuleScript")
-		s.Name = "UserSnippets"
-		s.Parent =  current[1]
+		local currentScript = current[1]
+		if currentScript:IsA("LuaSourceContainer") then
+			while true do
+				task.wait(.05)
+				if (currentScript.Source == '--class') then
+					currentScript.Source = [[
+local class = {}
+class.__index = class
 
-		plugin:OpenScript(s)
+function class.new(x, y, z)
+	local self = {}
+	self.x = x
+	self.y = y
+	self.z = z
 
-		while true do
-			wait(0.1)
-			if s.Source == "class" then
-				s.Source = "local Class = {}\nClass.__index = Class\n\nfunction Class.new()\n\tlocal self = setmetatable({\n\t\t\n\t}, Class)\n\treturn self\n end\n\nfunction Class:method(a: string)\n\treturn print(a)\nend\n\nreturn Class"
-				s.Name = "Class"
-				wait(0.05)
+	return setmetatable(self, class)
+end
 
-				f(s)
-				warn(s, "has been converted to: Class")
-			end
+function class:print()
+	return print(self.x, self.y, self.z)
+end
 
-			if s.Source == "oop" then
-				s.Source = "local oop = {}\noop.__index = oop\noop.ClassName = 'UserSnippets'\n\nfunction oop.new()\n\treturn setmetatable({}, oop)\nend\n\nfunction oop:create(a, b)\n\treturn self.a, self.b or string.upper(a), string.upper(b)\nend\n\nfunction oop:doSomething()\n\treturn print(self.Name)\nend\n\nfunction oop:method()\n\treturn self.Name\nend\n\nreturn oop"
-				s.Name = "OOP"
-				wait(0.05)
+function class:combine()
+	return self.x+self.y+self.z
+end
 
-				f(s)
-				warn(s, " has been converted to: OOP")
-			end
+return class
+]]
+					currentScript.Name = "Class"
+				end
 
-			if s.Source == "map" then
-				s.Source = "local module = {}\nmodule.__index = module\n\nfunction f()\n\t\nend\n\nreturn module"
-				s.Name = "Map"
-				wait(0.05)
+				if (currentScript.Source == '--oop') then
+					currentScript.Source = [[
+local OOP = {}
+OOP.__index = OOP
 
-				f(s)
-				warn(s, "has been converted to: Mapping(s)")
-			end
+function OOP.new()
+	local self = {}
+	self.statement = "object oriented programming"
+	return setmetatable(self, OOP)
+end
 
-			if s.Source == "afg" then
-				s.Source = "local controller = {}\n\nfunction controller:Start()\n\t\nend\n\nfunction controller:Init()\n\t\nend\n\nreturn controller"
-				s.Name = "Aero"
-				wait(0.05)
+function OOP:statement()
+	return self.statement
+end
 
-				f(s)
-				warn(s, " has been converted to: AFG_Controller")
-			end
-
-			if s.Source == "setget" then
-				s.Source = "local lets = {}\nlets.__index = lets\n\nfunction lets:set(a: any, b: any?)\n\treturn a, b\nend\n\nfunction lets:get(a)\n\treturn self.a\nend\n\nreturn lets"
-				s.Name = "Lets"
-				wait(0.05)
-
-				f(s)
-				warn(s, " has been converted to: Setters & Getters")
+return OOP
+]]
+					currentScript.Name = "OOP"
+				end
 			end
 		end
 	end
 end
 
-function settingMain()
-
-end
-
-genScript.Click:Connect(main)
-packScript.Click:Connect(settingMain)
+activation.Click:Connect(main)
 
